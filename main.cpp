454c317ce92045002a4fd0629aeb2b807bb4c688
naufalbintang <naufalbintangpradana@gmail.com>
2025-06-02 20:54:15 +0700

    register

--- main.cpp
+++ main.cpp
@@ -1,24 +1,85 @@
 #include <iostream>
 using namespace std;
 
-void login(){
-    string username;
-    string password;
-    cout << "Username: "; cin >> username;
-    cout << "Password: "; cin >> password;
-    if(username == "admin" && password == "admin123"){
-        cout << "Login sebagai admin" << endl;
+// hash function
+struct Node{
+    string username{};
+    string password{};
+    Node* next;
+
+    Node(string usn, string pass){
+        username = usn;
+        password = pass;
+        next = nullptr;
+    }
+};
+
+const int TABLESIZE = 10;
+Node* hashTable[TABLESIZE] = {};
+
+int hashFuction(string password){
+    return tolower(password[0]) % TABLESIZE;
+}
+
+void insertHash(string username, string password){
+    int index = hashFuction(password);
+    Node* newNode = new Node(username, password);
+
+    for(int i{}; i < TABLESIZE; i++){
+        Node* travel = hashTable[i];
+        while(travel != nullptr){
+            if(username == travel->username){
+                cout << "Username sudah terdaftar" << endl;
+                return;
+            }
+            travel = travel->next;
+        }
+    }
+
+    if(hashTable[index] == nullptr){
+        hashTable[index] = newNode;
+    }else{
+        newNode->next = hashTable[index];
+        hashTable[index] = newNode;
     }
 }
 
+void printHash(){
+    cout << endl;
+    for(int i{}; i < TABLESIZE; i++){
+        cout << "[" << i << "]";
+        Node* travel = hashTable[i];
+        while(travel != nullptr){
+            cout << " (" << travel->username << ", " << travel->password << ") ->";
+            travel = travel->next;
+        }
+        cout << "nullptr" << endl;
+    }
+    cout << endl;
+}
+
+void login(){
+    cout << "Login" << endl;
+}
+
 void registrasi(){
-    cout << "Registrasi" << endl;
+    string username{};
+    string password{};
+
+    cout << "===================" << endl;
+    cout << "  MENU REGISTRASI" << endl;
+    cout << "===================" << endl;
+    cout << "Masukkan username: "; cin >> username;
+    cout << "Masukkan password: "; cin >> password;
+
+    insertHash(username, password);
 }
 
 int main(){
     // variabel
     char pilihan;
-    
+    bool exit = false;
+
     // clear console
     #ifdef _WIN32
         system("cls");
@@ -27,20 +88,31 @@ int main(){
     #endif
 
     // main program
-    cout << "============" << endl;
-    cout << "    BANK" << endl;
-    cout << "============" << endl;
-    cout << "1. Login" << endl;
-    cout << "2. Register" << endl;
-    cout << "Masukkan pilihan: "; cin >> pilihan;
-    switch(pilihan){
-    case '1':
-        login();
-        break;
-    case '2':
-        registrasi();
-        break;
-    default:
-        break;
+    while(!exit){
+        cout << "============" << endl;
+        cout << "    BANK" << endl;
+        cout << "============" << endl;
+        cout << "1. Login sebagai admin" << endl;
+        cout << "2. Login sebagai nasabah" << endl;
+        cout << "3. Register" << endl;
+        cout << "0. Keluar" << endl;
+        cout << "Masukkan pilihan: "; cin >> pilihan;
+        switch(pilihan){
+        case '0':
+            exit = true;
+            break;
+        case '1':
+            login();
+            break;
+        case '3':
+            registrasi();
+            break;
+        case '4':
+            printHash();
+            break;
+        default:
+            cout << "Input tidak valid" << endl;
+            break;
+        }
     }
 }
